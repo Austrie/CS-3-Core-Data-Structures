6f76ae156a528b8b06e951f5b110b501b0ebe1e8
Shane Austrie <yoyo.rom.dev@gmail.com>
2018-03-03 22:34:04 -0800

    Queue DST: Completed implementation of LinkedList-backed queue, and started working on DynamicArray-backed queue.

--- source/queue.py
+++ source/queue.py
@@ -39,7 +39,10 @@ class LinkedQueue(object):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
-        return self.list.get_at_index(0)
+        if self.length() is 0:
+            return None
+        else:
+            return self.list.get_at_index(0)
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
@@ -50,7 +53,8 @@ class LinkedQueue(object):
             raise ValueError("Queue is empty!")
 
         head = self.front()
-        return self.list.delete(head)
+        self.list.delete(head)
+        return head
 
 
 # Implement ArrayQueue below, then change the assignment at the bottom
@@ -72,26 +76,31 @@ class ArrayQueue(object):
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
         # TODO: Check if empty
+        return True if len(self.list) is 0 else False
 
     def length(self):
         """Return the number of items in this queue."""
         # TODO: Count number of items
+        return len(self.list)
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
+        # return self.list
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return front item, if any
+        # Hard to make this O(1) without making it a circular buffer
 
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
